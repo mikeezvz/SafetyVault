a76985d605985d6c4d0cf8d93dd97d90e84142a3
mikeezvz <mike.zogheib@outlook.com>
2025-01-14 21:45:53 +0100

    add: SpringBoot backend

---
+++ safetyvault-be/src/main/java/ch/bbw/safetyvault_be/SafetyvaultBeApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.safetyvault_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyvaultBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyvaultBeApplication.class, args);
+	}
+
+}
---
+++ safetyvault-be/src/test/java/ch/bbw/safetyvault_be/SafetyvaultBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.safetyvault_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyvaultBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
