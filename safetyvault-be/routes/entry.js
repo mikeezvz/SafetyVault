167a3b56f317149a744c9503afa28fc211ded418
mikeezvz <mike.zogheib@outlook.com>
2025-01-15 13:56:35 +0100

    update: Improved entry.js

--- safetyvault-be/routes/entry.js
+++ safetyvault-be/routes/entry.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const entrySchema = new mongoose.Schema({
     service: { type: String, required: true },
@@ -16,24 +17,33 @@ router.get('/all', async (request, response) => {
         response.json(entries);
         console.log('Data received')
     } catch (error) {
-        response.status(500).send('Tasks can not be loaded from DB')
+        response.status(500).send('Entries can not be loaded from DB')
         console.error('Data can not be received:', error)
     }
 });
 
 router.post('/new', async (request, response) => {
     try {
+        const { service, username, password } = request.body;
+        if (!service || !username || !password) {
+            return response.status(400).json({ message: 'All fields are required' });
+        }
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
         const doc = new Entry({
-            service: request.body.service,
-            description: request.body.description,
-            completed: false
+            service: service,
+            username: username,
+            password: hashedPassword,
+            user: req.session.userId
           });
-        const savedTask = await doc.save();
-        response.status(201).json(savedTask)
+        const savedEntry = await doc.save();
+        response.status(201).json(savedEntry)
         console.log('Document saved')
     } catch (error) {
-    
+        response.status(500).send('Entry can not be saved')
+        console.error('Error saving Entry:', error)
     }
 });
+
+
 
 module.exports = router;
